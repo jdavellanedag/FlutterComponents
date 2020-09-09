8a053d9ccc46ea1d87ccbcd04306cadea42c5724
jdavellanedag <jdavellanedag@gmail.com>
2020-09-09 18:32:53 -0500

    Alert page started

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Avatar Page"),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://i.blogs.es/195f61/linus1/1366_2000.jpg'),
+              radius: 20.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage(
+              'https://i.pcmag.com/imagery/articles/040JHoVNgc1gh2e7sunj82k-1.fit_scale.size_1050x591.v1569492349.png'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
